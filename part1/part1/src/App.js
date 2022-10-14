2a0543990fd86b206b45c6bad5bea704d964b5e4
Terry Mak <fmak.1536@gmail.com>
2022-10-14 10:28:10 +0800

    complete ex 1.3-1.5

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -19,21 +19,29 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+      name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercise: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content />
-      <Total />
+      <Content parts={parts}/>
+      <Total parts={parts}/>
     </div>
   )
 }
